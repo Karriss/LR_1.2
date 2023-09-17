1887abe92f9d3114a373cb3047beba103139cb38
Karriss <144677707+Karriss@users.noreply.github.com>
2023-09-17 22:43:10 +0300

    Add files via upload

---
+++ блок 4(4).pas
@@ -0,0 +1,18 @@
+﻿program ChessAttack;
+var
+rookX, rookY, targetX, targetY: integer;
+begin
+writeln('Введите координаты ладьи (x, y):');
+readln(rookX);
+readln(rookY);
+
+writeln('Введите координаты фигуры, которую нужно проверить (x, y):');
+readln(targetX);
+readln(targetY);
+
+// Проверка для ладьи
+if (rookX = targetX) or (rookY = targetY) then
+writeln('Ладья бьет фигуру.')
+else
+writeln('Ладья не бьет фигуру.');
+end.
